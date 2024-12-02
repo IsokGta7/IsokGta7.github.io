7750bff439769cc0841ac2b7344423b152652967
IsokGta7 <isakzon0202@gmail.com>
2024-12-02 03:47:11 -0700

    Test

--- assets/YOLO/script_real-time.js
+++ assets/YOLO/script_real-time.js
@@ -3,17 +3,19 @@ var videoWidth = 320;  // Resolución reducida para dispositivos móviles
 var videoHeight = 240;
 var fpsLimit = 10;  // Limitar los FPS a 10 (100 ms entre cada iteración)
 
-// Función para dibujar el rectángulo
-function drawRectangle(y, x, w, h, lbl, iter) {
-    var box = $('.box').clone();
-    box.find('.box_label').text(lbl);
-    box.addClass('cl' + iter);
-    box.css('display', 'block');
-    box.css('right', x);
-    box.css('top', y);
-    box.css('width', w);
-    box.css('height', h);
-    $(".real_time").append(box);
+// Función para dibujar el rectángulo en el canvas
+function drawRectangle(ctx, y, x, w, h, lbl) {
+    // Dibujar el rectángulo
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+
+    // Escribir la etiqueta
+    ctx.font = '16px Arial';
+    ctx.fillStyle = 'red';
+    ctx.fillText(lbl, x, y > 10 ? y - 5 : 10);
 }
 
 // Variables globales
@@ -23,6 +25,8 @@ var time_start;
 var time_end;
 var doLoop = true;
 var lastDetectionTime = 0; // Para limitar la frecuencia de detección
+var canvas = document.querySelector("#videoCanvas");
+var ctx = canvas.getContext("2d");
 
 // Función para mostrar la webcam
 function showWebcam() {
@@ -33,6 +37,7 @@ function showWebcam() {
                 .then(function (stream) {
                     video.srcObject = stream;
                     doLoop = true;
+                    video.play();
                 })
                 .catch(function (err0r) {
                     console.log("Something went wrong!");
@@ -43,6 +48,12 @@ function showWebcam() {
         doLoop = true;
         realTimeYOLO();
     }
+}
+
+// Función para dibujar el video en el canvas
+function drawVideoOnCanvas() {
+    var video = document.querySelector("#webcam_feed");
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);  // Dibuja el video en el canvas
 }
 
 // Función para detección en tiempo real
@@ -64,10 +75,24 @@ function realTimeYOLO() {
         iter++;
         $('.time').text('Processing time: ' + Number(time_end - time_start).toString() + 'ms');
         $('.objno').text('Objects detected: ' + results.length);
+
+        // Limpiar el canvas antes de dibujar el nuevo frame
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Dibujar el video en el canvas
+        drawVideoOnCanvas();
+
+        // Dibujar los rectángulos y las etiquetas sobre el canvas
         for (var i = 0; i < results.length; i++) {
             var lbl = results[i].className + ' (' + Math.round(results[i].classProb * 100) + '%)';
-            drawRectangle(results[i].y * height, results[i].x * width, results[i].w * width, results[i].h * height, lbl, iter);
+            var y = results[i].y * height;
+            var x = results[i].x * width;
+            var w = results[i].w * width;
+            var h = results[i].h * height;
+
+            drawRectangle(ctx, y, x, w, h, lbl);  // Dibuja sobre el canvas
         }
+
         if (doLoop) {
             requestAnimationFrame(realTimeYOLO);  // Usar requestAnimationFrame para optimizar el rendimiento
         }
